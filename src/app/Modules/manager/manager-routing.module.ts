f190d823995d13721ff946a444e2180cc1205fd6
Divya <divyashree@amazeinc.in>
2023-12-17 23:09:12 +0530

    dd

--- src/app/Modules/manager/manager-routing.module.ts
+++ src/app/Modules/manager/manager-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BuildingComponent } from './building/building.component';
 
 const routes: Routes = [
     
-    { path: 'Building', component: BuildingComponent },
 ];
 
 @NgModule({
--- src/app/Modules/manager/manager.module.ts
+++ src/app/Modules/manager/manager.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BuildingComponent } from './building/building.component';
 
 
 
 @NgModule({
   declarations: [
-    BuildingComponent
   ],
   imports: [
     CommonModule
